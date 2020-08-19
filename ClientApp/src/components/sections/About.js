92921f049f856667c9c09e5226a0da5b4c049e15
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-08-19 20:40:02 +0200

    new photo of mine

--- ClientApp/src/components/sections/About.js
+++ ClientApp/src/components/sections/About.js
@@ -3,8 +3,8 @@ import { Section } from '../utilities/Section';
 
 export function About({ nextSectionName }) {
     const info = {
-        photo: 'images/photo.png',
-        paragraphs: ["Apart from studying and programing, I am definitely a sport loving person. My main passion is powerlifting, which started in 2015 as a casual gym visit, and developed into national level competitive journey.",
+        photo: 'images/photo.jpg',
+        paragraphs: ["Apart from studying and programming, I am definitely a sport loving person. My main passion is powerlifting, which started in 2015 as a casual gym visit, and developed into national level competitive journey.",
             "I enjoy spending my spare time biking or discovering  new types of craft beer along with my friends, and personally, I can't imagine a better day start, but a one, that begins with a cup of black coffee accompanied by a sci-fi novel."],
     }
 
