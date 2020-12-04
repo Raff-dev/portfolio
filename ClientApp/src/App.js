0ec0321806e0e2884d457654d1b23d0dc1b9e6ae
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-12-04 16:46:07 +0100

    remove unnecessary imports

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,14 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { NavMenu } from './components/sections/NavMenu';
 import { Sidebar } from './components/utilities/Sidebar';
 import { Home } from './components/sections/Home';
 import { Projects } from './components/sections/Projects';
-import { Education } from './components/sections/Education';
-import { MdTest } from './components/sections/MdTest';
 import { About } from './components/sections/About';
 import { Skills } from './components/sections/Skills';
-import { Experience } from './components/sections/Experience';
 import './css/App.scss';
 import './css/Carousel.scss';
 import './css/Home.scss';
@@ -21,7 +18,7 @@ import './css/Contact.scss';
 import { Contact } from './components/sections/Contact';
 
 export const App = () => {
-    const sections = ['Home', 'Markdown', 'Projects', 'Skills', 'About', 'Contact']
+    const sections = ['Home', 'Projects', 'Skills', 'About', 'Contact']
 
     return (
         <div className="p-0">
@@ -29,13 +26,11 @@ export const App = () => {
             <NavMenu sections={sections} />
             <Home nextSectionName={sections[1]} />
             <div>
-                <MdTest nextSectionName={sections[2]} />
+                <Projects nextSectionName={sections[2]} />
                 <hr className="m-0" />
-                <Projects nextSectionName={sections[3]} />
+                <Skills nextSectionName={sections[3]} />
                 <hr className="m-0" />
-                <Skills nextSectionName={sections[4]} />
-                <hr className="m-0" />
-                <About nextSectionName={sections[5]} />
+                <About nextSectionName={sections[4]} />
                 <hr className="m-0" />
                 <Contact nextSectionName={null} />
             </div>
