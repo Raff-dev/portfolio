d8e141245786db578404140382c004ff7b37fe02
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-12-04 12:35:49 +0100

    fixed section ordering

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { NavMenu } from './components/sections/NavMenu';
+import { Sidebar } from './components/utilities/Sidebar';
 import { Home } from './components/sections/Home';
 import { Projects } from './components/sections/Projects';
 import { Education } from './components/sections/Education';
@@ -20,18 +21,21 @@ import './css/Contact.scss';
 import { Contact } from './components/sections/Contact';
 
 export const App = () => {
-    const sections = ['Home', 'Projects', 'Skills', 'About', 'Contact']
+    const sections = ['Home', 'Markdown', 'Projects', 'Skills', 'About', 'Contact']
 
     return (
         <div className="p-0">
+            <Sidebar sections={sections} />
             <NavMenu sections={sections} />
-            <Home />
+            <Home nextSectionName={sections[1]} />
             <div>
-                <Projects nextSectionName={sections[2]} />
+                <MdTest nextSectionName={sections[2]} />
                 <hr className="m-0" />
-                <Skills nextSectionName={sections[3]} />
+                <Projects nextSectionName={sections[3]} />
                 <hr className="m-0" />
-                <About nextSectionName={sections[4]} />
+                <Skills nextSectionName={sections[4]} />
+                <hr className="m-0" />
+                <About nextSectionName={sections[5]} />
                 <hr className="m-0" />
                 <Contact nextSectionName={null} />
             </div>
